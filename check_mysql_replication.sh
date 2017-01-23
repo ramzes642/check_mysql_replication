f08865de8038fa43f71f8bc8cc179f5b9c6b1ac8
ramzes642 <ramzeska@gmail.com>
2017-01-23 19:44:48 +0300

    Check slave only by "seconds behind master" field

--- check_mysql_replication.sh
+++ check_mysql_replication.sh
@@ -52,7 +52,7 @@ while :; do
             SLAVEHOST=$2
             shift
             ;;
-        *) 
+        *)
             echo "Unknown argument: $1"
             echo $usage
             exit $STATE_UNKNOWN
@@ -73,16 +73,6 @@ then
     exit $STATE_CRITICAL
 fi
 
-master_status_file=`mktemp`
-master_error_file=`mktemp`
-master_connection_check=`mysql -e "show master status" >$master_status_file 2>$master_error_file`
-if [[ $? -ne 0 ]]
-then
-    echo "Error reading master: $(cat $master_error_file)"
-    rm -f $master_error_file
-    exit $STATE_CRITICAL
-fi
-
 #Replace Read_Master_Log_Pos By  Exec_Master_Log_Pos for comparaison purpose
 #iSlave_1_position=`grep bin $slave_status_file | cut -f7`
 iSlave_1_position=`grep bin $slave_status_file | cut -f22`
@@ -90,31 +80,35 @@ iSlave_1_status=`grep bin $slave_status_file | cut -f1`
 iSlave_1_Slave_IO_Running=`grep bin $slave_status_file | cut -f11`
 iSlave_1_Slave_SQL_Running=`grep bin $slave_status_file | cut -f12`
 iSlave_1_Last_Errno=`grep bin $slave_status_file | cut -f19`
+iSlave_1_Seconds_Behind_Master=`grep bin $slave_status_file | cut -f33`
 rm -f $slave_status_file
 
-iMaster_position=`grep bin $master_status_file | cut -f2`
-rm -f $master_status_file
 
-iDiff_1=`expr $iMaster_position - $iSlave_1_position`
 
-if [ $iDiff_1 -gt $REPL_DIFFERENCE ]
-then
-    echo "CRITICAL - master log $iMaster - slave log $iSlave_1 - log positions differ by more than $REPL_DIFFERENCE"
-    exit $STATE_CRITICAL
-elif [ "$iSlave_1_status" != "Waiting for master to send event" ]
-then
-    echo "CRITICAL - slave status is '$iSlave_1_status'"
-    exit $STATE_CRITICAL
-#NEW CHECK
-elif [[ "$iSlave_1_Slave_IO_Running" != "Yes" || "$iSlave_1_Slave_SQL_Running" != "Yes" || "$iSlave_1_Last_Errno" != "0" ]]
+
+
+if [[ "$iSlave_1_Slave_IO_Running" != "Yes" || "$iSlave_1_Slave_SQL_Running" != "Yes" || "$iSlave_1_Last_Errno" != "0" ]]
 then
 echo "CRITICAL - slave '$SLAVEHOST' IO/SQL RUNNING / Last_Errno: is not at 0 : IO RUNNING->'$iSlave_1_Slave_IO_Running' / SQL RUNNING->'$iSlave_1_Slave_SQL_Running' / Last_Errno->'$iSlave_1_Last_Errno'"
     rm_ifexist
     exit $STATE_CRITICAL
 #END NEW CHECK
+fi
+
+if [ $iSlave_1_Seconds_Behind_Master -gt 60 ]
+then
+    echo "WARNING - slave $iSlave_1_Seconds_Behind_Master seconds behind master"
+    exit $STATE_WARNING
 else
-    echo "OK - log positions match ($iMaster_position = $iSlave_1_position), slave status = '$iSlave_1_status'"
+    echo "OK - Seconds behind ($iSlave_1_Seconds_Behind_Master), slave status = '$iSlave_1_status'"
     exit $STATE_OK
+fi
+
+if [ "$iSlave_1_status" != "Waiting for master to send event" ]
+then
+    echo "CRITICAL - slave status is '$iSlave_1_status'"
+    exit $STATE_CRITICAL
+#NEW CHECK
 fi
 
 exit $STATE_CRITICAL
